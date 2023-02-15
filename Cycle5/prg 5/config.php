471b58e85879109d33c313285956ac1148d07320
Denzel-Sunny <125428975+Denzel-Sunny@users.noreply.github.com>
2023-02-15 14:18:09 +0530

    Add files via upload

---
+++ Cycle5/prg 5/config.php
@@ -0,0 +1,9 @@
+<?php
+$mysql_host="localhost";
+$mysql_user="22mca022";
+$mysql_password="2017";
+$conn=mysqli_connect($mysql_host,$mysql_user,$mysql_password);
+if(mysqli_select_db($conn,'22mca022'))
+{echo 'connected';}
+else{echo 'falied';}
+?>
